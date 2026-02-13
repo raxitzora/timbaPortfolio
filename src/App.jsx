613056984b5b8410116acc875f1e49b4355a0329
raxitzora <raxitzora@gmail.com>
2026-02-13 17:12:38 +0530

    started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,7 +6,8 @@ function App() {
   return (
     <>
      <div>
-      
+      hello
+
      </div>
     </>
   )
